be86f3788229e525b1daaa8fa445858edd550c7a
Tor Borgen <Tor Borgen>
2017-03-08 15:22:45 +0100

    More treassure!

--- mainTreasure.go
+++ mainTreasure.go
@@ -1,15 +1,28 @@
 package main
 
 import (
-	"path/filepath"
+	"fmt"
+	"strings"
 
-	"./treasure"
+	"./fileutils"
 )
 
 func main() {
 
-	hexPath, _ := filepath.Abs("/GitHub/org/is105-ica03/treasure/treasure.txt")
+	//hexPath, _ := filepath.Abs("/GitHub/org/is105-ica03/treasure/treasure.txt")
 	//fmt.Printf("%s", fileutils.FileToByteslice(hexPath))
-	treasure.PrintTreasureUTF8(hexPath)
+	//treasure.PrintTreasureUTF8(hexPath)
+	byteSlice := fileutils.FileToByteslice("/GitHub/org/is105-ica03/treasure/treasure.txt")
+	//fmt.Println(byteSlice)
+	//fmt.Printf("%s", byteSlice)
+	//fmt.Printf("%q", byteSlice)
+	s := fmt.Sprintln(byteSlice)
+	fmt.Println(s)
+	s := strings.Trim(s, "\\x")
+	fmt.Printf("%c", 120)
+
+	//bs, _ := hex.DecodeString(string(byteSlice))
+	//fmt.Println(bs)
+	//treasure.PrintTreasureUTF8(hexPath)
 
 }
